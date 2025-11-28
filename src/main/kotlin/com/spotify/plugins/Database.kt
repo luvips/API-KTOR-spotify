1e4ee805e4e40762b0a3622c93b52d7a11585c7f
luvia <luviaamagaali07@gmail.com>
2025-11-27 23:37:43 -0600

    Conexión a BD y creación automática de tablas

---
+++ src/main/kotlin/com/spotify/plugins/Database.kt
@@ -0,0 +1,35 @@
+package com.spotify.plugins
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import com.spotify.repository.Users // Importamos la tabla
+
+fun Application.configureDatabases() {
+    val config = environment.config.config("storage")
+    val driverClassName = config.property("driverClassName").getString()
+    val jdbcUrl = config.property("jdbcUrl").getString()
+    val username = config.property("username").getString()
+    val password = config.property("password").getString()
+
+    val hikariConfig = HikariConfig().apply {
+        this.driverClassName = driverClassName
+        this.jdbcUrl = jdbcUrl
+        this.username = username
+        this.password = password
+        maximumPoolSize = 3
+        isAutoCommit = false
+        transactionIsolation = "TRANSACTION_REPEATABLE_READ"
+        validate()
+    }
+
+    val database = Database.connect(HikariDataSource(hikariConfig))
+
+    // Crear tabla Users si no existe
+    transaction(database) {
+        SchemaUtils.create(Users)
+    }
+}
