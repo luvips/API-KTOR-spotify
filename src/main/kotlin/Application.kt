ea86bbe54fa4c05952eef227070fe72b52996ed5
luvia <luviaamagaali07@gmail.com>
2025-11-29 22:49:26 -0600

    feat:aplicar el arranque de la aplicación

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -2,38 +2,19 @@ package com.spotify
 
 import io.ktor.server.application.*
 import io.ktor.server.netty.*
-import io.ktor.server.routing.*
 import com.spotify.plugins.*
-import com.spotify.routes.*
 import com.spotify.services.*
-
-
-
 
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
-    // 1. Plugins
     configureSerialization()
     configureDatabases()
-    configureSecurity()
 
+    val artistService = ArtistService()
+    val albumService = AlbumService()
+    val songService = SongService()
 
-
-    // 2. Servicios
-    val secret = environment.config.property("jwt.secret").getString()
-    val issuer = environment.config.property("jwt.domain").getString()
-    val audience = environment.config.property("jwt.audience").getString()
-    val authService = AuthService(secret, issuer, audience)
-
-    val s3Service = S3Service(environment.config)
-    val artistService = ArtistService(s3Service)
-
-    val albumService = AlbumService(s3Service)
-
-    val songService = SongService(s3Service)
-
-    // 3. Rutas
-    configureRouting(authService, artistService, albumService, songService)
+    // Llama a la función de enrutamiento principal
+    configureRouting(artistService, albumService, songService)
 }
-
